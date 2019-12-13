e4312534109f7b3c999b68579b5280e935339e8b
Serena Chen <serenadantingchen@gmail.com>
2019-12-13 17:02:05 +1300

    a bunch of stuff

--- _plugins/generate-project_page.rb
+++ _plugins/generate-project_page.rb
@@ -1,23 +1,16 @@
+# # Generate project pages from directory structure
+
 # module Jekyll
+
 #   class ProjectPageGenerator < Generator
 #     safe true
 
 #     def generate(site)
 
-#       indexpage = site.pages.detect {|page| page.name == 'index.html'}
-#       indexpage.data['ongoing'] = ongoing
-
-
-#       if site.layouts.key? 'project'
-#         dir = site.config['project_dir'] || 'projects'
-#         site.projects.each_key do |project|
-#           site.pages << ProjectPage.new(site, site.source, File.join(dir, project), project)
-#         end
-#       end
+      
 #     end
 #   end
 
-#   # A Page subclass used in the `ProjectPageGenerator`
 #   class ProjectPage < Page
 #     def initialize(site, base, dir, project)
 #       @site = site
@@ -33,4 +26,5 @@
 #       self.data['title'] = "#{project_title_prefix}#{project}"
 #     end
 #   end
+
 # end
